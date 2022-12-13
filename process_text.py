f87eaf95f61e2b7c15ba3b2fbd710c666b8f5cde
Haley Johnson <77760202+haleyej@users.noreply.github.com>
2022-12-12 21:02:50 -0500

    final version

--- process_text.py
+++ process_text.py
@@ -1,10 +1,11 @@
-# Code for final project for STATS 315
-# Cleans and preprocesses text
+# Statistics and Artifical Intelligence, at the University of Michigan
+# Cleans and preprocesses text in the dataset used in my final project
 # Haley Johnson
 
 import pandas as pd
 from langdetect import DetectorFactory, detect_langs, detect
 import emoji
+from nltk.corpus import stopwords
 
 def get_lang(s):
     DetectorFactory.seed = 42
@@ -21,8 +22,17 @@ def remove_emojis(s):
             valid += c
     return valid
 
-def clean_text(df):
+def clean_text(df, stopwords):
+    '''
+    Preprocess text 
+
+    Removes emojis, non-english tweets, non-ascii characters, stopwords
+    and some other text features 
+
+    Makes calls to remove_emojis and get_lang 
+    '''
   # filter out non-english tweets
+    df.message = df.message.str.lower()
     df['message_lang'] = df.message.apply(get_lang)
     df.message_lang = df.message_lang.apply(lambda s: [l.lang for l in s if l.lang == 'en' and l.prob >= 0.90 and type(l) != 'str'])
     df = df[df.message_lang.apply(lambda s: 'en' in s)]
@@ -37,49 +47,38 @@ def clean_text(df):
     df.message_cleaned = df.message_cleaned.str.replace("RT\s@\S+?:\s", "", regex = True)
     df.message_cleaned = df.message_cleaned.str.replace("^(@\S+\s)+?", "", regex = True)
     df.message_cleaned = df.message_cleaned.str.replace("@", "", regex = True)
+    
     #truncate links
     df.message_cleaned = df.message_cleaned.str.replace('https?:\S+', 'https', regex = True)
 
     #remove hashtags 
     df.message_cleaned = df.message_cleaned.str.replace("#", "", regex = True)
 
-    #get rid of quotations 
-    df.message_cleaned = df.message_cleaned.str.replace("'", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace('"', '', regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("''", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace('‘', "", regex = True)
-
     #more punctuation 
     df.message_cleaned = df.message_cleaned.str.replace("…", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace('.', "", regex = True)
     df.message_cleaned = df.message_cleaned.str.replace('�', "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("?", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("!", "", regex = True)
     df.message_cleaned = df.message_cleaned.str.replace("&amp", " ", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace(":", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace(";", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("-", " ", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("–", " ", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("$", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace("(", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace(")", "", regex = True)
-    df.message_cleaned = df.message_cleaned.str.replace(",", "", regex = True)
 
     #fix white space
     df.message_cleaned = df.message_cleaned.str.replace('\s\s+', ' ', regex = True)
     df.message_cleaned = df.message_cleaned.str.lstrip()
 
-    df['message_cleaned'] = df['message_cleaned'].astype('str')
+    # remove stopwords
+    no_stop_words = [word for word in df.message_cleaned.str.split() if word not in stopwords]
+    df.message_cleaned = [" ".join(tweet) for tweet in no_stop_words]
+
+    df.message_cleaned = df['message_cleaned'].astype('str')
 
     return df 
 
 def main():
-    df = pd.read_csv("data/twitter_sentiment.csv")
+    df = pd.read_csv("data/twitter_sentiment_data.csv")
     df = df.dropna(subset=['message', 'sentiment'])
 
     df.sentiment = df.sentiment + 2
+    stop_words = stopwords.words('english')
 
-    df = clean_text(df)
+    df = clean_text(df, stop_words)
     df.to_csv("data/cleaned_text.csv")
 
 if __name__ == "__main__":
